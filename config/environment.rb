4e72f82d65d52f96bfe7c9a85f4f8770dc519bdd
Justin Breneman <justin.breneman@yahoo.com>
2020-07-09 21:41:29 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,3 @@ require_all 'lib'
 
 module Concerns
 end
-
-#require_relative '../lib/concerns/findable.rb'
-
-#require_relative '../lib/artist.rb'
-#require_relative '../lib/genre.rb'
-#require_relative '../lib/song.rb'
